ff6f56c0e168e6531f631ac9b37646fc503f60cc
{自åtokoro <99924975+tokorokoro@users.noreply.github.com>
2022-04-23 17:50:28 +0900

    update TopファイルにweatherAPIから取得したデータをresultsStateに記録し、Resultファイルに流した

--- src/components/organisms/Result/Result.tsx
+++ src/components/organisms/Result/Result.tsx
@@ -1,3 +1,13 @@
-export const Result = () => {
-  return <h1>気象データ</h1>;
+type ResultsStateType = {
+  results: {
+    country: string;
+    cityName: string;
+    temperature: string;
+    conditionText: string;
+    icon: string;
+  };
+};
+
+export const Result = (props: ResultsStateType) => {
+  return <h1>気象データ{props.results.country}</h1>;
 };
--- src/components/pages/Top/Top.tsx
+++ src/components/pages/Top/Top.tsx
@@ -4,19 +4,45 @@ import { Form } from "../../organisms/Form/Form";
 import { Result } from "../../organisms/Result/Result";
 import { Title } from "../../organisms/Title/Title";
 
+type ResultsStateType = {
+  country: string;
+  cityName: string;
+  temperature: string;
+  conditionText: string;
+  icon: string;
+};
+
 export const Top = () => {
   const [city, setCity] = useState<string>("");
+  const [results, setResults] = useState<ResultsStateType>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: "",
+  });
   const getWeather = (e: any) => {
     e.preventDefault();
     fetch(
       "http://api.weatherapi.com/v1/current.json?key=f90c2b6ca2714b188c975827222304&q=London&aqi=no"
-    ).then((res) => res.json().then((data) => console.log(data)));
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setResults({
+          country: data.location.country,
+          cityName: data.location.name,
+          temperature: data.current.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.current.condition.icon,
+        });
+      });
   };
+
   return (
     <>
       <Title />
       <Form setCity={setCity} getWeather={getWeather} />
-      <Result />
+      <Result results={results} />
     </>
   );
 };
