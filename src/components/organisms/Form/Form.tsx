ad1afc120656c979792dc599457bba9e81e42942
{自åtokoro <99924975+tokorokoro@users.noreply.github.com>
2022-04-23 17:21:17 +0900

    update FormファイルにてweatherAPIから仮で情報を取得出来るようにした

--- src/components/organisms/Form/Form.tsx
+++ src/components/organisms/Form/Form.tsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
 export const Form = () => {
+  const [city, setCity] = useState<string>("");
+  const getWeather = (e: any) => {
+    e.preventDefault();
+    fetch(
+      "http://api.weatherapi.com/v1/current.json?key=f90c2b6ca2714b188c975827222304&q=London&aqi=no"
+    ).then((res) => res.json().then((data) => console.log(data)));
+  };
+
   return (
     <form>
-      <input type="text" name="city" placeholder="都市名" />
-      <button type="submit">Get Weather</button>
+      <input
+        type="text"
+        name="city"
+        placeholder="都市名"
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button type="submit" onClick={getWeather}>
+        Get Weather
+      </button>
     </form>
   );
 };
