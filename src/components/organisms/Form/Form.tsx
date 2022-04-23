f10a2ef80de6ffd3db42e92950d35f13e46c00e8
{自åtokoro <99924975+tokorokoro@users.noreply.github.com>
2022-04-23 16:52:46 +0900

    add Formファイルを作成し、Topにimportした

---
+++ src/components/organisms/Form/Form.tsx
@@ -0,0 +1,8 @@
+export const Form = () => {
+  return (
+    <form>
+      <input type="text" name="city" placeholder="都市名" />
+      <button type="submit">Get Weather</button>
+    </form>
+  );
+};
--- src/components/pages/Top/Top.tsx
+++ src/components/pages/Top/Top.tsx
@@ -1,9 +1,11 @@
+import { Form } from "../../organisms/Form/Form";
 import { Title } from "../../organisms/Title/Title";
 
 export const Top = () => {
   return (
     <>
       <Title />
+      <Form />
     </>
   );
 };
