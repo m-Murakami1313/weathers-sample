3a300778d3329127dbae73c1108d94a4af6b55fb
{自åtokoro <99924975+tokorokoro@users.noreply.github.com>
2022-04-23 16:46:32 +0900

    add AppにTopファイルをimport

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Top } from "./components/pages/Top/Top";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Top />
+    </div>
+  );
 }
 
 export default App;
